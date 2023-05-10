f9c5cbd4fc886fe20f95bee487b85a042907edd4
Vlad Epov <vladep622@gmail.com>
2023-05-10 12:48:13 +0300

    made some preliminary mail cmps, connected them, and colored them to distinguish  between them

---
+++ apps/mail/cmps/mail-nav-side-bar.jsx
@@ -0,0 +1,14 @@
+export function MailNavSideBar(){
+    return(
+        <section className="mail-nav-side-bar">
+            <h2>Mail Side Nav Bar Here</h2>
+            <section>
+                <article>Inbox</article>
+                <article>Starred</article>
+                <article>Sent</article>
+                <article>Draft</article>
+                <article>Trash</article>
+            </section>
+        </section>
+    )
+}
--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,5 +1,17 @@
+import { MailList } from "../cmps/mail-list.jsx"
+import { MailNavSideBar } from "../cmps/mail-nav-side-bar.jsx"
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+
+    return (
+        <section className="mail-index">
+            <h1>Hello Mail App</h1>
+            <section>
+                <MailNavSideBar />
+                <MailList />
+            </section>
+        </section>
+    )
 }
 
