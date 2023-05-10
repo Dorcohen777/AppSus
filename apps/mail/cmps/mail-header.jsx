a82ef832376f761289ba8525402a1b33595d5be4
Vlad Epov <vladep622@gmail.com>
2023-05-10 13:17:22 +0300

    added mail header

---
+++ apps/mail/cmps/mail-header.jsx
@@ -0,0 +1,5 @@
+export function MailHeader() {
+    return (
+        <section className="mail-header">Hello Mail Header</section>
+    )
+}
--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,16 +1,17 @@
 import { MailList } from "../cmps/mail-list.jsx"
 import { MailNavSideBar } from "../cmps/mail-nav-side-bar.jsx"
+import { MailHeader } from "../cmps/mail-header.jsx"
+
+
 
 export function MailIndex() {
 
 
     return (
         <section className="mail-index">
-            <h1>Hello Mail App</h1>
-            <section>
-                <MailNavSideBar />
-                <MailList />
-            </section>
+            <MailHeader />
+            <MailNavSideBar />
+            <MailList />
         </section>
     )
 }
