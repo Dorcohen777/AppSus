9463572e1046bc8aac9e56a6f77ae996c2336496
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-10 13:15:05 +0300

    added demo function in note.service change name of export in asyn-storage

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,4 +1,26 @@
+const { useEffect, useState } = React
+const { Link, useSearchParams } = ReactRouterDOM
+
+import { noteService } from '../services/note.service.js'
 
 export function NoteIndex() {
-    return <div>note app</div>
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [notes, setNotes] = useState([])
+    const [filterBy, setFilterBy] = useState(noteService.getDefaultFilter(searchParams))
+
+    useEffect(() => {
+        loadNotes()
+    }, [filterBy])
+
+    function loadNotes() {
+        noteService.query(filterBy)
+            .then(notes => setNotes(notes))
+    }
+
+    return (
+        <main>
+            <h1>Hello from note app</h1>
+        </main>
+    )
 }
