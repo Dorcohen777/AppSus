00e4b0447ebfcdb84da7e2636856b5722f735991
Vlad Epov <vladep622@gmail.com>
2023-05-10 17:36:43 +0300

    added dates and buttons, what is displayed on mail preview changes upon mail preview hover

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,3 +1,4 @@
+import { utilService } from "../../../services/util.service.js"
 import { mailService } from "../services/mail.service.js"
 
 const { useEffect } = React
@@ -9,14 +10,24 @@ export function MailPreview({ mail }) {
 
     }, [])
 
+
+    function renderDate(sentAt) {
+        const date = new Date(sentAt)
+        return `${date.getDate()} ${utilService.getMonthName(date)}`
+    }
+
     return (
         <tr>
             <td>
                 <section className="mail-title"><span>{mail.subject}</span></section>
                 <section className="mail-content"><span>{mail.body}</span></section>
                 <section className="mail-date-buttons">
-                    <article className="mail-date"><span>date</span></article>
-                    <article className="mail-buttons"><span>buttons</span></article>
+                    <article className="mail-date"><span>{renderDate(mail.sentAt)}</span></article>
+                    <article className="mail-buttons"><span>
+                        <i className="fa-solid fa-box-archive"></i>
+                        <i className="fa-solid fa-trash"></i>
+                        <i className="fa-solid fa-envelope-open"></i>
+                    </span></article>
                 </section>
             </td>
         </tr>
