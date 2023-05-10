4ecc75d961eabd4d4d2286933162094c6d02a7b1
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-10 17:11:00 +0300

    added note-create.jsx to handle new notes

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -4,8 +4,6 @@ import { NotePreview } from "./note-preview.jsx"
 
 export function NoteList({ notes }) {
 
-    console.log('notes from NoteList', notes)
-
     return (
         <ul className="ul-notes">
 
@@ -14,11 +12,11 @@ export function NoteList({ notes }) {
                     <section className="section-notes" >
                         <NotePreview note={note} />
                         <div className="div-actions">
-                            <button><i class="fa-solid fa-trash"></i></button>
-                            <button><i class="fa-solid fa-pen-to-square"></i></button>
-                            <button><i class="fa-solid fa-palette"></i></button>
-                            <button><i class="fa-solid fa-thumbtack"></i></button>
-                            <button><i class="fa-solid fa-clone"></i></button>
+                            <button><i className="fa-solid fa-trash"></i></button>
+                            <button><i className="fa-solid fa-pen-to-square"></i></button>
+                            <button><i className="fa-solid fa-palette"></i></button>
+                            <button><i className="fa-solid fa-thumbtack"></i></button>
+                            <button><i className="fa-solid fa-clone"></i></button>
                         </div>
                     </section>
                 </li>
