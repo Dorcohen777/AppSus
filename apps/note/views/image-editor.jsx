53ea2a6210cb18f2e3d751852adc05f2d4d62923
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-11 20:44:02 +0300

    css update of editor and buttons in notes

--- apps/note/views/image-editor.jsx
+++ apps/note/views/image-editor.jsx
@@ -11,15 +11,17 @@ export function ImageEditor({ onEditSubmit, handleEditChange, currNote }) {
     }
 
     return (
-        <div className="editor-panel" ref={panelRef}>
-            <button className="btn-exit" onClick={() => onExitClick()}>X</button>
-            <form onSubmit={onEditSubmit} className="editor-form">
-                <h2>Enter new title</h2>
-                <input type="text" name="txt" onChange={handleEditChange} value={currNote.info.title} />
-                <h2>Enter new url</h2>
-                <input type="url" name="url" onChange={handleEditChange} value={currNote.info.url} />
-                <button>Save</button>
-            </form>
-        </div >
+        <section>
+            <div className="editor-panel" ref={panelRef}>
+                <button className="btn-exit" onClick={() => onExitClick()}>X</button>
+                <form onSubmit={onEditSubmit} className="editor-form">
+                    <h2>Enter new title</h2>
+                    <input type="text" name="txt" onChange={handleEditChange} value={currNote.info.title} />
+                    <h2>Enter new url</h2>
+                    <input type="url" name="url" onChange={handleEditChange} value={currNote.info.url} />
+                    <button>Save</button>
+                </form>
+            </div >
+        </section>
     )
 }
--- apps/note/views/text-editor.jsx
+++ apps/note/views/text-editor.jsx
@@ -1,4 +1,4 @@
-const {useRef } = React
+const { useRef } = React
 
 
 
@@ -11,13 +11,15 @@ export function TextEditor({ onEditSubmit, handleEditChange, currNote, setEditNo
     }
 
     return (
-        <div className="editor-panel" ref={panelRef}>
-            <button className="btn-exit" onClick={() => onExitClick()}>X</button>
-            <form onSubmit={onEditSubmit} className="editor-form">
-                <h2>Enter new text</h2>
-                <input type="text" name="txt" onChange={handleEditChange} placeholder='enter new text' value={currNote.info.txt} />
-                <button>Save</button>
-            </form>
-        </div>
+        <section>
+            <div className="editor-panel" ref={panelRef}>
+                <button className="btn-exit" onClick={() => onExitClick()}>X</button>
+                <form onSubmit={onEditSubmit} className="editor-form">
+                    <h2>Enter new text</h2>
+                    <input type="text" name="txt" onChange={handleEditChange} placeholder='enter new text' value={currNote.info.txt} />
+                    <button>Save</button>
+                </form>
+            </div>
+        </section>
     )
 }
