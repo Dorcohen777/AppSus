15d9fd8211bcbf68b267525acc6ced0128873a3e
Vlad Epov <vladep622@gmail.com>
2023-05-11 19:26:19 +0300

    MAIL - added some color and styled the left navbar

--- apps/mail/cmps/mail-nav-side-bar.jsx
+++ apps/mail/cmps/mail-nav-side-bar.jsx
@@ -6,8 +6,7 @@ export function MailNavSideBar({ onAddMail }) {
 
     return (
         <section className="mail-nav-side-bar">
-            <h2>Mail Side Nav Bar Here</h2>
-            <section className="btnLook" onClick={onAddMail}>New Mail</section>
+            <section className="btnLook btnNewMail" onClick={onAddMail}>New Mail</section>
             <section className="mail-nav-sidebar-links">
                 <NavLink className="btnLook" to="/mail/inbox" >Inbox</NavLink>
                 <NavLink className="btnLook" to="/mail/starred" >Starred</NavLink>
