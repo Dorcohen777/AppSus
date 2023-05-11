0cb5b731b3fb31d6769d1071981e5ddd71c6bd92
Vlad Epov <vladep622@gmail.com>
2023-05-11 20:26:35 +0300

    fixed trash functionality

--- apps/mail/cmps/mail-header.jsx
+++ apps/mail/cmps/mail-header.jsx
@@ -1,5 +1,8 @@
 export function MailHeader() {
     return (
-        <section className="mail-header">Hello Mail Header</section>
+        <section className="mail-header">
+            <article>Gmail Icon Here</article>
+            <article>input type search here</article>
+        </section>
     )
 }
--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -87,7 +87,6 @@ export const mailService = {
 
 
 
-
 function query(filterBy = {}) {
     return asyncStorageService.query(MAIL_KEY)
         .then(mails =>
@@ -110,27 +109,20 @@ function save(mail) {
     }
 }
 
+
 function changeMailState(id, state = {}) {
     console.log('state', state)
     console.log('id', id)
     return get(id)
         .then(prevMail => {
-            if (state.status === 'trash') state[trashedAt] = Date.now()
-            const editedMail = ({ ...prevMail, ...state })
+            const editedMail = ({ ...prevMail, ...state, removedAt: (state.status === 'trash') ? Date.now() : null })
             console.log('editedMail', editedMail)
             return save(editedMail)
         })
         .catch(err => {
             console.log('err Could not Get mail', err)
         })
-        // .then(mail => {
-        //     console.log('edited mail', mail)
-        // })
-        // .catch(err => {
-        //     console.log('err Could not save mail', err)
-        // })
 }
-
 
 function getEmptyNewMail() {
     return {
