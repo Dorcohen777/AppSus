e7a955c828fac3bb785daaefab54dbb2ce44d561
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-12 13:12:39 +0300

    upadte home page design, header design, added logo

--- views/home.jsx
+++ views/home.jsx
@@ -1,6 +1,21 @@
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <div className="div-header">
+            <div className="div-header-text">
+
+                <h1 className="animate__animated animate__fadeInDown">AppSus Gmail and Google Keeper Clone: Your All-in-One Email and Note-Taking Solution</h1>
+                <h2 className="animate__animated animate__fadeIn animate__delay-1s">Experience the Power of Gmail and Google Keeper in One Seamless Platform: Efficiently Manage Your Emails and Notes With User-Friendly Clone</h2>
+                <button className="btn-mail animate__animated animate__fadeIn animate__delay-1s">Mail App</button>
+                <button className="btn-keeper animate__animated animate__fadeIn animate__delay-1s">Keeper App</button>
+                <h3 className="animate__animated animate__fadeInUp animate__delay-2s">Created by Dor & Vlad</h3>
+
+            </div>
+
+
+            <div className="div-header.image">
+                <img src="../assets/img/logo/logo.png" className="img-logo animate__animated animate__rotateInDownRight animate__delay-1s" />
+            </div>
+        </div>
     </section>
 }
