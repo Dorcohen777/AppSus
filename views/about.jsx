53988bf09bac4c6bc4d826a72955cc3018b2f5e5
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-12 14:12:49 +0300

    added about page animation

--- views/about.jsx
+++ views/about.jsx
@@ -1,16 +1,16 @@
 
 export function About() {
     return <section className="about">
-        <h1 className="h1-about">Welcome to the About page of AppSus</h1>
-        <h2 className="h2-about">Your all-in-one email and note-taking solution!</h2>
-        <p className="p-about">
-            AppSus is a website that aims to provide a seamless platform for managing your emails and notes. <br/> <br/> With its user-friendly interface, you can easily access and organize your emails and notes in one place. <br/> <br/> 
+        <h1 className="h1-about animate__animated animate__fadeInDown animate__delay-1s">Welcome to the About page of AppSus</h1>
+        <h2 className="h2-about animate__animated animate__fadeInDown animate__delay-2s">Your all-in-one email and note-taking solution!</h2>
+        <p className="p-about animate__animated animate__fadeIn animate__delay-3s">
+            AppSus is a website that aims to provide a seamless platform for managing your emails and notes. <br /> <br /> With its user-friendly interface, you can easily access and organize your emails and notes in one place. <br /> <br />
 
-            The website is a clone of Gmail and Google Keeper app, two of the most popular productivity apps by Google. <br/> <br/> created using React - a popular JavaScript library for building user interfaces. <br/> <br/>
+            The website is a clone of Gmail and Google Keeper app, two of the most popular productivity apps by Google. <br /> <br /> created using React - a popular JavaScript library for building user interfaces. <br /> <br />
 
-            We wanted to provide users with the convenience of having two essential apps in one place. <br/> <br/> With AppSus, you can efficiently manage your emails and notes without the need to switch between apps or platforms. <br/> <br/>
+            We wanted to provide users with the convenience of having two essential apps in one place. <br /> <br /> With AppSus, you can efficiently manage your emails and notes without the need to switch between apps or platforms. <br /> <br />
 
-            Our website is equipped with various features such as the ability to compose, send, and receive emails, as well as create, edit, and delete notes. <br/> <br/>
+            Our website is equipped with various features such as the ability to compose, send, and receive emails, as well as create, edit, and delete notes. <br /> <br />
 
             Thank you for choosing AppSus, and we hope you enjoy using our website!
         </p>
