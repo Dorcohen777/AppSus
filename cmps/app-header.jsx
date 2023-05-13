797d58e5aecb605cb7640a3690efc15c27625ec7
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-05-13 15:04:05 +0300

    added hamburget menu, also fix responsive in home page

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -2,17 +2,28 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
+    function toggleMenu() {
+        document.body.classList.toggle('menu-open')
+        document.querySelector('.app-header nav').classList.toggle('show')
+    }
+
     return <header className="app-header">
         <Link to="/">
             <img src="assets/img/logo/logo.png" />
             <h3>AppSus</h3>
         </Link>
-        <nav>
+        <nav className="main-nav">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
             <NavLink to="/note">Book</NavLink>
             <NavLink to="/about">About</NavLink>
         </nav>
+        <div className="hamburger-menu" onClick={() => toggleMenu()}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </header>
+
 }
